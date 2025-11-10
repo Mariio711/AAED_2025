6d90b2e50b3a867f6ef572597d28b1a96167caac
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2025-11-10 16:28:57 +0100

    Agregar clase polinomio con métodos para manejar coeficientes y grados

---
+++ Practica3new/poli_user.h
@@ -0,0 +1,26 @@
+#ifndef POLI_USER_H_
+#define POLI_USER_H_
+#include <cstdlib>
+
+class polinomio
+{
+public:
+    // Constructor
+    explicit polinomio(unsigned gradoMax = 0) : pol(new double[gradoMax + 1]), GradoMax(gradoMax), Grado(0) {}
+
+    unsigned gradoMax() const { return GradoMax; };
+    unsigned grado() const { return Grado; };
+
+    double coeficiente(unsigned n) const { return (n > Grado) ? 0 : pol[n]; };
+    void coeficiente(unsigned n, double c)
+    {
+        if (n <= GradoMax)
+            pol[n] = c;
+    };
+
+private:
+    double *pol;
+    unsigned GradoMax, Grado;
+};
+
+#endif
